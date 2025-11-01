35347f0bc2d799172b8c8655019cb6ee09850b06
ZhouSicheng-2011 <ZSCinYBSZ2023@outlook.com>
2025-11-01 22:46:06 +0800

    update

---
+++ lib7z.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+using namespace std;
+#include <bit7z.hpp>
+#include <bitarchiveeditor.hpp>
+#include <bitarchivereader.hpp>
