01a3b32d034a270078d957475f88ef9d304be4b1
ZhouSicheng-2011 <ZSCinYBSZ2023@outlook.com>
2025-11-09 12:34:09 +0800

    Update lib7z.hpp

--- lib7z.hpp
+++ lib7z.hpp
@@ -2,7 +2,12 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <map>
+#include <chrono>
 using namespace std;
 #include <bit7z.hpp>
 #include <bitarchiveeditor.hpp>
 #include <bitarchivereader.hpp>
+#include <bitarchiveitem.hpp>
+#include <bitarchiveiteminfo.hpp>
+using namespace bit7z;
