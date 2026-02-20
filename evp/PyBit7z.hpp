4cd5c170f5b49d0ab4f1b34bc7c42ca5e0236ebd
ZhouSicheng-2011 <ZSCinYBSZ2023@outlook.com>
2026-02-20 21:57:47 +0800

    update

--- evp/PyBit7z.hpp
+++ evp/PyBit7z.hpp
@@ -1,8 +1,6 @@
 /*
-This is the main wrapper of bit7z for Python 3.8 or greater (pybind11-3.0.2)
+This is the main wrapper of bit7z for Python 3.8 or higher versions (pybind11-3.0.2)
 */
-#ifndef PYBIT7Z_HPP
-#define PYBIT7Z_HPP
 
 /*
 If you want to use this program on Windows 7, please add "/DWIN7_COMPAT" opition in cl compiler command
@@ -13,9 +11,8 @@ If you want to use this program on Windows 7, please add "/DWIN7_COMPAT" opition
 #endif
 
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
 #include <pybind11/chrono.h>
 #include <pybind11/gil.h>
 namespace py = pybind11;
 
-#endif
